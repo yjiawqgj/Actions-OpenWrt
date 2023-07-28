1f330b52236fa9febca6eee25c5812684841e13f
yjiawqgj <71002956+yjiawqgj@users.noreply.github.com>
2023-07-28 20:19:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,14 @@
 sed -i 's/10.10.10.3/192.168.1.1/g' package/base-files/files/bin/config_generate
 # git clone https://github.com/kenzok8/small-package package/small-package
 git clone https://github.com/sirpdboy/sirpdboy-package package/sirpdboy-package
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# openclash
+mkdir package/others/openclash
+cd package/others/openclash
+git init
+git remote add origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+cd ../../..
