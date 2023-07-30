2758ab256f58a2b29e93f1652bf88a43145d28bf
yjiawqgj <71002956+yjiawqgj@users.noreply.github.com>
2023-07-30 18:25:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,4 +16,4 @@ sed -i 's/10.10.10.3/192.168.1.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/sirpdboy/sirpdboy-package package/sirpdboy-package
 # git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/yjiawqgj/luci-app-netmap package/luci-app-netmap
-git clone https://github.com/vi-si/luci-app-openclash package/luci-app-openclash
+
