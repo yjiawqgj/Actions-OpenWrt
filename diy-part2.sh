56e0d16db99b258f4d56351206f20af381be69cb
yjiawqgj <71002956+yjiawqgj@users.noreply.github.com>
2023-07-31 10:13:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.3/g' package/base-files/files/bin/config_generate
 # git clone https://github.com/kenzok8/small-package package/small-package
-git clone https://github.com/sirpdboy/sirpdboy-package package/sirpdboy-package
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/yjiawqgj/luci-app-netmap package/luci-app-netmap
-git clone https://github.com/vi-si/luci-app-openclash package/openclash
+git clone https://github.com/kenzok8/small-package package/small-package
