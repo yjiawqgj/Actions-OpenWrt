b55f8fcc3b044a696949a1922d1b8861587ce4ea
yjiawqgj <71002956+yjiawqgj@users.noreply.github.com>
2023-07-31 10:13:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.3/g' package/base-files/files/bin/config_generate
-# git clone https://github.com/kenzok8/small-package package/small-package
 git clone https://github.com/kenzok8/small-package package/small-package
+
